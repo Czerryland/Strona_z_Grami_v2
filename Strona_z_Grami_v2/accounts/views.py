4e78a8470152eb6a2744470bb4e78cc9bb6bad6c
Grzegorz <grzes.labedzki@gmail.com>
2025-04-18 17:04:45 +0200

    Wykonanie css dla formularza rejestrującego, użytkownicy zapisują sie w bazie danych

--- Strona_z_Grami_v2/accounts/views.py
+++ Strona_z_Grami_v2/accounts/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def signupview(request):
- form = UserCreationForm()
- return render(request, 'signup.html',{'form':form})
+ if request.method == 'POST':
+  form = UserCreationForm(request.POST)
+  if (form.is_valid()):  
+    form.save()
+    return redirect("mainpage")
+ else:
+  form = UserCreationForm()
+  return render(request, 'signup.html',{'form':form})
