6c34ee9731da8e4eaf960249a3c082cfad2cfbad
Grzegorz <grzes.labedzki@gmail.com>
2025-04-18 21:13:57 +0200

    Dodanie formularza logujacego uzytkownikow, oraz zapisywanie kiedy uzytkownik jest zalogowany

--- Strona_z_Grami_v2/accounts/urls.py
+++ Strona_z_Grami_v2/accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signupview, name='signup'),
+    path('login/', views.loginview, name='login'),
+    path('logout/', views.logoutview, name='logout'),
 ]
--- Strona_z_Grami_v2/accounts/views.py
+++ Strona_z_Grami_v2/accounts/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def signupview(request):
  if request.method == 'POST':
   form = UserCreationForm(request.POST)
-  if (form.is_valid()):  
-    form.save()
+  if form.is_valid():  
+    user = form.save()
+    login(request, user)
     return redirect("mainpage")
  else:
   form = UserCreationForm()
-  return render(request, 'signup.html',{'form':form})
+ return render(request, 'signup.html',{'form':form})
+ 
+def loginview(request):
+ if request.method == 'POST':
+  form = AuthenticationForm(data=request.POST)
+  if form.is_valid():
+   #login
+   user = form.get_user()
+   login(request, user)
+   return redirect("mainpage")
+ else:
+  form = AuthenticationForm()
+ return render(request, "login.html", {'form':form})
+
+def logoutview(request):
+ if request.method == 'POST':
+  logout(request)
+  return redirect("mainpage")
